5a7740d05007f15679c4044863e0806fd66c46e2
Larry Shen <larry.shen@nxp.com>
2021-12-04 13:12:21 +0800

    Add lava online management command.
    Signed-off-by: Larry Shen <larry.shen@nxp.com>

---
+++ management/cmd_online_lava_devices.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import asyncio
+import logging
+import traceback
+import yaml
+
+import common as _
+from core.config import Config
+from core.plugin import AsyncRunMixin
+
+
+class LavaManagement(AsyncRunMixin):  # pylint: disable=too-few-public-methods
+    def __init__(self):
+        self.managed_resources = Config.managed_resources
+        self.lava_identities = Config.frameworks_config["lava"]["identities"]
+
+    async def __lava_recover(self, resource):
+        cmd = (
+            f"lavacli -i {self.lava_identities} devices update --health GOOD {resource}"
+        )
+        await self._run_cmd(cmd)
+
+    async def action(self):
+        candidate_managed_resources = []
+
+        cmd = f"lavacli -i {self.lava_identities} devices list --yaml"
+        _, devices_text, _ = await self._run_cmd(cmd)
+        try:
+            devices = yaml.load(devices_text, Loader=yaml.FullLoader)
+            for device in devices:
+                if device["hostname"] in self.managed_resources:
+                    if device["health"] not in ("Unknown", "Good"):
+                        candidate_managed_resources.append(device["hostname"])
+        except yaml.YAMLError:
+            logging.error(traceback.format_exc())
+
+        recover_tasks = [
+            self.__lava_recover(resource) for resource in candidate_managed_resources
+        ]
+        await asyncio.gather(*recover_tasks)
+
+        logging.info("Recover lava devices done.")
+
+
+if __name__ == "__main__":
+    asyncio.run(LavaManagement().action())
