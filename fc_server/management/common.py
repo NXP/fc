35f1a36ff024715ed1d2bad5b8d812fa9db4a345
Larry Shen <larry.shen@nxp.com>
2022-01-10 17:18:31 +0800

    Remove hack path append.
    Signed-off-by: Larry Shen <larry.shen@nxp.com>

--- fc_server/management/common.py
+++ fc_server/management/common.py
@@ -1,9 +1,4 @@
 import logging
-import os
-import sys
-
-fc_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-sys.path.append(fc_path)
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
