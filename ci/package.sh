ac702c9c4c649c725f98df6b3f50f3ed76bea6d0
Larry Shen <larry.shen@nxp.com>
2022-01-11 11:33:05 +0800

    ci: add pypi package script.
    Signed-off-by: Larry Shen <larry.shen@nxp.com>

---
+++ ci/package.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -x
+
+declare ROOT_HOME="$(cd "$(cd "$(dirname "$0")"; pwd -P)"/..; pwd)"
+
+python3 $ROOT_HOME/setup.py clean
+python3 $ROOT_HOME/setup.py sdist fc-server
+python3 $ROOT_HOME/setup.py sdist fc-guarder
+python3 $ROOT_HOME/setup.py sdist fc-client
