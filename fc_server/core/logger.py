9b47f8579431aa194460a63e05fe3d8c3cd645f3
Larry Shen <larry.shen@nxp.com>
2022-01-12 10:47:25 +0800

    Allow user specify log folder.
    Signed-off-by: Larry Shen <larry.shen@nxp.com>

--- fc_server/core/logger.py
+++ fc_server/core/logger.py
@@ -10,7 +10,7 @@ import logging
 class Logger:
     @staticmethod
     def init(fc_path):
-        log_path = os.path.join(fc_path, "log")
+        log_path = os.environ.get("FC_CONFIG_PATH", os.path.join(fc_path, "log"))
         log_file = os.path.join(log_path, "run.log")
 
         if not os.path.exists(log_path):
