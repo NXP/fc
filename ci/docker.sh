72401e366aebf8b9d0eadebd1dd6d76d49ca4765
Larry Shen <larry.shen@nxp.com>
2022-01-13 13:44:04 +0800

    Add docker build to ci.
    Signed-off-by: Larry Shen <larry.shen@nxp.com>

---
+++ ci/docker.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -x
+
+declare ROOT_HOME="$(cd "$(cd "$(dirname "$0")"; pwd -P)"/..; pwd)"
+
+version=$(cat $ROOT_HOME/fc_common/VERSION)
+
+cd $ROOT_HOME
+
+docker build -t atline/fc-client:$version -f docker/fc_client/Dockerfile .
+docker tag atline/fc-client:$version atline/fc-client
+docker build -t atline/fc-server:$version -f docker/fc_server/Dockerfile .
+docker build -t atline/fc-guarder:$version -f docker/fc_guarder/Dockerfile .
