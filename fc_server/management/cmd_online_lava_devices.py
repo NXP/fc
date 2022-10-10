bc1ab6eba17b1358f448022af891867270d95633
Larry Shen <larry.shen@nxp.com>
2022-10-10 10:20:13 +0800

    guarder: use dedicated lava function.
    Signed-off-by: Larry Shen <larry.shen@nxp.com>

--- fc_server/management/cmd_online_lava_devices.py
+++ fc_server/management/cmd_online_lava_devices.py
@@ -25,44 +25,26 @@
 
 import asyncio
 import logging
-import traceback
-import yaml
 
 import fc_server.management.common as _
-from fc_server.core import AsyncRunMixin
 from fc_server.core.config import Config
+from fc_server.plugins.utils.lava import Lava
 
 
-class LavaManagement(AsyncRunMixin):  # pylint: disable=too-few-public-methods
+class LavaManagement(Lava):  # pylint: disable=too-few-public-methods
     def __init__(self):
         self.managed_resources = Config.managed_resources
-        self.lava_identities = Config.frameworks_config["lava"]["identities"]
-
-    async def __lava_recover(self, resource):
-        cmd = (
-            f"lavacli -i {self.lava_identities} devices update --health GOOD {resource}"
-        )
-        await self._run_cmd(cmd)
+        self.identities = Config.frameworks_config["lava"]["identities"]
 
     async def action(self):
-        candidate_managed_resources = []
-
-        cmd = f"lavacli -i {self.lava_identities} devices list --yaml"
-        _, devices_text, _ = await self._run_cmd(cmd)
-        try:
-            devices = yaml.load(devices_text, Loader=yaml.FullLoader)
-            for device in devices:
-                if device["hostname"] in self.managed_resources:
-                    if device["health"] not in ("Unknown", "Good"):
-                        candidate_managed_resources.append(device["hostname"])
-        except yaml.YAMLError:
-            logging.error(traceback.format_exc())
-
-        recover_tasks = [
-            self.__lava_recover(resource) for resource in candidate_managed_resources
+        maintenance_devices = [
+            device["hostname"]
+            for device in await self.lava_get_devices()
+            if device["hostname"] in self.managed_resources
+            and device["health"] in ("Maintenance",)
         ]
-        await asyncio.gather(*recover_tasks)
 
+        await self.lava_online_devices(*maintenance_devices)
         logging.info("Recover lava devices done.")
 
 
