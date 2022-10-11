8165997b9a50eec536df04ab34849eefabd7410d
Larry Shen <larry.shen@nxp.com>
2022-10-11 10:24:34 +0800

    guarder: reset all frameworks' control before online devices.
    Signed-off-by: Larry Shen <larry.shen@nxp.com>

--- fc_server/management/cmd_online_lava_devices.py
+++ fc_server/management/cmd_online_lava_devices.py
@@ -26,12 +26,24 @@
 import asyncio
 import logging
 
+from importlib import import_module
+
 import fc_server.management.common as _
 from fc_server.core.config import Config
 from fc_server.plugins.utils.lava import Lava
 
 
 class LavaManagement(Lava):
+    def __init__(self):
+        super().__init__()
+
+        self.__framework_plugins = [
+            import_module("fc_server.plugins." + framework).Plugin(
+                Config.frameworks_config[framework]
+            )
+            for framework in Config.registered_frameworks
+        ]
+
     async def __get_device_description(self, device):
         device_info = await self.lava_get_device_info(device)
         return (
@@ -55,6 +67,17 @@ class LavaManagement(Lava):
                 )
             ]
 
+            # reset all frameworks' control on that device
+            for framework in self.__framework_plugins:
+                if framework.__module__.split(".")[-1] != "lava":
+                    await asyncio.gather(
+                        *[
+                            framework.force_kick_off(device)
+                            for device, _ in recover_devices
+                        ]
+                    )
+
+            # online lava device
             await asyncio.gather(
                 *[
                     self.lava_online_devices(
