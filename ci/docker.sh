1f9d87bf14f14541d4e97903fb2bdd711bc33f1d
Larry Shen <larry.shen@nxp.com>
2023-06-26 16:21:46 +0800

    Don't use build cache for docker image publish
    Signed-off-by: Larry Shen <larry.shen@nxp.com>

--- ci/docker.sh
+++ ci/docker.sh
@@ -8,7 +8,7 @@ version=$(cat $ROOT_HOME/fc_common/VERSION)
 
 cd $ROOT_HOME
 
-docker build -t atline/fc-client:$version -f docker/fc_client/Dockerfile .
+docker build -t atline/fc-client:$version -f docker/fc_client/Dockerfile . --no-cache
 docker tag atline/fc-client:$version atline/fc-client
-docker build -t atline/fc-server:$version -f docker/fc_server/Dockerfile .
-docker build -t atline/fc-guarder:$version -f docker/fc_guarder/Dockerfile .
+docker build -t atline/fc-server:$version -f docker/fc_server/Dockerfile . --no-cache
+docker build -t atline/fc-guarder:$version -f docker/fc_guarder/Dockerfile . --no-cache
